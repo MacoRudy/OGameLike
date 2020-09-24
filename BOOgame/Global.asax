adb61cdc07c2dff5889a6bffd17aa5145e076b87
Rudy MACORIGH <rudy.macorigh2020@campus-eni.fr>
2020-09-24 11:15:52 +0200

    Initial Commit

--- BOOgame/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BOOgame.MvcApplication" Language="C#" %>
--- BOOgame/Properties/AssemblyInfo.cs
+++ BOOgame/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de 
-// l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("BOOgame")]
+[assembly: AssemblyTitle("BOOGame")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BOOgame")]
+[assembly: AssemblyProduct("BOOGame")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
-// COM, affectez la valeur true à l’attribut ComVisible sur ce type.
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
-// Le GUID suivant est pour l’ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("1a3c28b9-21e2-4581-a8bb-9659b0288fbf")]
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("03ca80cb-5f24-4f16-8898-6aa5cd559be1")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
 //      Version principale
-//      Version mineure
+//      Version secondaire
 //      Numéro de build
 //      Révision
 //
-// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les valeurs Révision et Numéro de version
-// en utilisant ’*’, comme indiqué ci-dessous :
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
